0af1172f8bed5e0c286f40eeaf4a1fc630ccc3a2
Terence Li <Terence Li>
2017-08-10 16:05:25 -0400

    Adding first component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'; // fetch the definition of Component from angular core (which contains core functionalities)
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})  // adding a decorator, and giving it a javascript object to configure it i.e. metadata 
+export class ServerComponent {
+
+}
