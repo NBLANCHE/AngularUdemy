e1670e5a0efb08936c33c8e906266c91f920e7f1
Terence Li <Terence Li>
2017-08-24 11:24:11 -0400

    ngstyle and ngclass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core'; // fetch the definition of Component 
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+    .online {
+        color:white;
+    }
+    
+    `]
 })  // adding a decorator, and giving it a javascript object to configure it i.e. metadata 
 export class ServerComponent {
     serverId = 10;
     serverStatus = 'offline';
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
     getServerStatus() {
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
